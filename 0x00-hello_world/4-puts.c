22f50bd04b65db8e3b6affd149d9dfd76da47b80
Timothy <iamchinweike001@gmail.com>
2023-06-15 18:55:58 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- prints out output to the stdout.
+ * Return: 0.
+ */
+
+int main(void)
+{
+
+char texToo[] = "\"Programming is like building a multilingual puzzle";
+
+puts(texToo);
+
+return (0);
+}
