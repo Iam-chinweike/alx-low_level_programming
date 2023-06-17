6022970137212f878287c6689f3d6b843ff19d08
Timothy <iamchinweike001@gmail.com>
2023-06-17 04:42:22 -0700

    understanding the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-prints rsult to the stdout.
+ *Return: 1
+ */
+
+int main(void)
+{
+int a;
+a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+putchar(int a);
+return (1);
+}
