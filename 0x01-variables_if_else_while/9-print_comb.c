741d20fef664c39e00ccfab0d618a70884a1742b
Timothy <iamchinweike001@gmail.com>
2023-06-19 07:25:48 -0700

    printing combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- prints possible combination of singlr digit numbers
+ * Return: 0 on success
+ */
+int main(void)
+{
+int deo;
+
+for (deo= 0; deo < 10; x++)
+{
+putchar(deo + '0');
+if (deo < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
