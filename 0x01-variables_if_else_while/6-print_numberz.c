d4661eab95e2996f6bbe70b3dcbe20749ae9f555
Timothy <iamchinweike001@gmail.com>
2023-06-19 05:10:16 -0700

    printing numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints  numbers of base ten with a loop function
+ * Return: 0 on success
+ */
+int main(void)
+{
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+return (0);
+}
