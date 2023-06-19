ecb0895c22cc38b42390d34914e2a3d290f83ff2
Timothy <iamchinweike001@gmail.com>
2023-06-19 04:41:31 -0700

    listint numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-prints numbers of base ten
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
