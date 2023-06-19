5518e63916fedbb6011de40b2d8cc76f406fb6c5
Timothy <iamchinweike001@gmail.com>
2023-06-19 04:19:37 -0700

    omitting some characters in the alphabet pool

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all alhabets except two alphabet
+ * Return: 0 on success
+ */
+int main(void)
+{
+char sheet;
+for (sheet = 'a'; sheet <= 'z'; sheet++)
+{
+if (sheet != 'j' && sheet != 'e')
+{
+putchar(sheet);
+}
+putchar('\n');
+}
+return (0);
+}
