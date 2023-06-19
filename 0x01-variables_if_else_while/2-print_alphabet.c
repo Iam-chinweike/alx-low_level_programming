05e8aa72e9148f873cd2c967efa33b862945d088
Timothy <iamchinweike001@gmail.com>
2023-06-19 03:42:55 -0700

    alphabet playground

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-prints out alphabets
+ * Return: 0 on success
+ */
+int main(void)
+{
+char z;
+for (z = 'a'; z <= 'z'; z++)
+{
+putchar(char z);
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main- prints the alphabets
+* Return: 0 on success
+*/
+
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(char a);
+}
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar(char a);
+}
+putchar('\n');
+return (0);
+}
