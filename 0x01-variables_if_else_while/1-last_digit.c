c393a770047aeec78cd13288fa76101fc7c453a7
Timothy <iamchinweike001@gmail.com>
2023-06-19 18:30:10 -0700

    fixed a bug that will not allow the third condition run correctly

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ if (n > 5)
 {
 printf("%d and is greater than 5\n", last_digit);
 }
-else if (n == 0)
+else if (n < 6 && != 0)
 {
-printf("%d and is 0\n", last_digit);
+printf("%d and is less than 6 and not 0\n", last_digit);
 }
 else
-printf("%d and is less than 6 and not 0\n", last_digit);
+printf("%d and is 0\n", last_digit);
 return (0);
 }
