933428c8e5340d1ca7b207a1374d5e6a173f6d6d
Timothy <iamchinweike001@gmail.com>
2023-06-19 19:28:18 -0700

    printing combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,10 +5,6 @@
  * The two digits must be different
  * 01 and 10 are considered the same combination of the two digits 0 and 1
  * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden) 
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
  * Return: 0 on success
  */
 int main(void)
