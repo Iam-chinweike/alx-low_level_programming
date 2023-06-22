d6c488b916e8b25290f1fa283955267dbdada95d
Timothy <iamchinweike001@gmail.com>
2023-06-21 21:06:04 -0700

    printing the absolute value

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign- prints out tge signs according to set criteria
+ *@n: is th variable being checked
+ *Return: values acording to their set conditions
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-')
+return (-1);
+}
+else
+{
+_putchar('0')
+return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - prints the absolute value
+ *@int: is the type of the argument
+ *Return: the absolute value
+ */
+int _abs(int)
+{
+if (d < 0)
+{
+int abval;
+abval = d * -1;
+return (abval);
+}
+return (d);
+}
