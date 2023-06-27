3ac76905168dfb6429364e1ceaa2b4b597f2e1e1
Timothy <iamchinweike001@gmail.com>
2023-06-26 22:01:07 -0700

    swapping values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
 /**
- * swap_int - changes the value of n to 98
+ * swap_init - changes the value of n to 98
  * @a: changes the value of a to b
  * @b: changes the value of b to a
+ * @temp: serves as a holder
  * Return: void on success
  */
 void swap_int(int *a, int *b)
 {
-*a = &b;
-*b = &a;
+int *temp;
+
+temp = a;
+*a = b;
+*b = temp;
 }
