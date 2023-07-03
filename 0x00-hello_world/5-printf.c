1f37d1160b25e2a6fe3f55db448b5bc94436244a
Timothy <iamchinweike001@gmail.com>
2023-07-03 16:28:03 -0700

    printing strings

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,12 +8,10 @@
 int main(void)
 {
 
-char quot1;
+char quot1[] = "with proper grammar, but the outcome is a piece of art,";
 
-quot1 = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", quot1);
 
-printf("%c\n", quot1);
-
-retun (0);
+return (0);
 
 }
