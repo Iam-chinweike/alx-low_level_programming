4e56e88e17db5b24da65bafaefa6777ff532e666
Timothy <iamchinweike001@gmail.com>
2023-07-03 17:05:49 -0700

    printing a quote to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <strings.h>
 
 /**
  *main-prints rsult to the stdout.
@@ -9,6 +10,6 @@ int main(void)
 {
 const char* a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-write(2, a);
+write(2, a, strlen(a));
 return (1);
 }
